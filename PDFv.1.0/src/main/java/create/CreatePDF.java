10d36b214dd00309c169f26240382ef30588648a
Andrew Kazantsev <asu-kazantsev@mail.ru>
2021-11-29 19:35:23 +0500

    Add and update methods with work table (addTable)

--- PDFv.1.0/src/main/java/create/CreatePDF.java
+++ PDFv.1.0/src/main/java/create/CreatePDF.java
@@ -5,7 +5,6 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.util.stream.Stream;
 
 import com.itextpdf.text.BadElementException;
 import com.itextpdf.text.BaseColor;
@@ -24,7 +23,7 @@ import com.itextpdf.text.pdf.PdfWriter;
  * <b> Класс реализующий создание и заполнение типового PDF файла.</b>
  * @author Kazantsev AV
  * @version 2.0
- * В классе один конструктор c 4 параметрами и 5 методами.
+ * В классе один конструктор c 2 параметрами.
  * Класс основан на библиотеки itextpdf.
  * 
  * Для создания PDF файла необходимо создать экземпляр объекта CreatePDF и далее добавлять необходимые вставки (текст, рисунки и таблицы). После окончания добавления необходимо выполнить метод getClose, который закроет и сохранить PDF документ.
@@ -38,25 +37,24 @@ private BaseFont times = null;
 /** Поле с именем создаваемого PDF файла */
 private String Namefile;
 /** Поле со значениями шапки таблицы*/
-private String[] arrayHat, NameCellHat;
-/** Поле с сслыкой на создаваеый документ */
+private String[] NameCellHat;
+private String[][] arrayCell;
+/** Поле с сслыкой на создаваемый документ */
 private Document document;
 /** Поле с количеством столбцов в таблице */
-private int Size;
+private int Size,SizeCell;
+/** */
+private PdfPTable table;
 
 /**
  * Конструктор - создание объекта с генерацией PDF
- * @param NameCellHat - массив с названиями в шапке таблицы
- * @param arrayHat - массив со значениями для шапки таблицы
  * @param Namefile - имя выводимого файла
  * @param BaseFontPDF - шрифт для вывода
  */
-	public CreatePDF(String[] NameCellHat, String[] arrayHat, String Namefile, BaseFont BaseFontPDF) { 
-		this.NameCellHat=NameCellHat;
-		this.arrayHat=arrayHat;
+	public CreatePDF(String Namefile, BaseFont BaseFontPDF) { 
 		this.Namefile=Namefile;
-		this.times=BaseFontPDF;
-		this.Size=NameCellHat.length;
+		this.times=BaseFontPDF;		
+		
 		this.document = new Document(); //создание объекта Document
 		try {
 			PdfWriter.getInstance(document, new FileOutputStream(this.Namefile)); //выходной поток для создания PDF, а внутри создается поток записи с конкретным именем
@@ -69,25 +67,30 @@ private int Size;
 	}
 
 	/** 
-	 * Метод добавления строк в таблицу {@link CreatePDF}
+	 * Метод добавления строк в таблицу
 	 * @param table - таблица для заполнения
+	 * @param arrayCell - двумерный массив со значения ячеек 
 	 */
 	
-private void addRows(PdfPTable table) {
+private void addRows(PdfPTable table, String[][] arrayCell) {
       //установка значения и шрифта для выводимого текста в ячейки
-        
-	for (int i=0; i<Size; i++) {
-		table.addCell(new Phrase(arrayHat[i], new Font(times,14)));
+	int SizeTwo = arrayCell.length; //количество строк
+	int SizeOne = arrayCell[0].length; //подразумеваем количество столбцов
+	
+   for (int j=0; j<SizeTwo; j++) {     
+	for (int i=0; i<SizeOne; i++) {
+		table.addCell(new Phrase(arrayCell[j][i], new Font(times,14)));
 	}
 	}
+   }
 
 	/**
-	 * Метод заполнения шапки таблицы {@link CreatePDF}
+	 * Метод заполнения шапки таблицы
 	 * @param table - таблица для заполнения
 	 */
 	
-private void setHeader(PdfPTable table) { //метод для работы с шапкой таблицы
-	    	for (int i=0; i<Size; i++) {  
+private void setHeader(PdfPTable table, String[] NameCellHat) { //метод для работы с шапкой таблицы
+	    	for (int i=0; i<NameCellHat.length; i++) {  
 	        PdfPCell header = new PdfPCell(); //реализация ячейки в таблице
 	        header.setBackgroundColor(BaseColor.LIGHT_GRAY);
 	        header.setBorderWidth(2);
@@ -98,7 +101,7 @@ private void setHeader(PdfPTable table) { //метод для работы с шапкой таблицы
 	}
 	
 /**
- * Метод для получения ссылка на создаваемый документ {@link CreatePDF}
+ * Метод для получения ссылка на создаваемый документ
  * @return возваращет ссылку на создаваемый документ
  */
 	
@@ -107,7 +110,7 @@ public Document getDocument() {
 }
 
 /**
- * Метод для закрытия и сохранения PDF файла {@link CreatePDF}
+ * Метод для закрытия и сохранения PDF файла
  */
 
 public void getClose() {
@@ -115,7 +118,7 @@ public void getClose() {
 }
 
 /**
- * Метод добавления картинки в PDF файл {@link CreatePDF}
+ * Метод добавления картинки в PDF файл
  * @param url - ссылка на изображение
  * @param document - ссылка на создаваемый документ
  * @param position1 - абсолютная позиция по оси X
@@ -177,22 +180,53 @@ public void addText(Document document, String Text, int SizeFont, boolean Space 
 }
 
 /**
- * Метод добавления таблицы в PDF документ.
+ * Метод инициализации таблицы и добавления шапки в таблице.
  * @param document - ссылка на создаваемый документ
+ * @param NameCellHat - данные для шапки таблицы
  */
 
-public void addTable (Document document) {
+public void InitTableAndAddHat (Document document, String[] NameCellHat) {
 	
-	PdfPTable table = new PdfPTable(Size);
-	setHeader(table); //задание заголовка (шапки таблицы)
-	addRows(table); // добавление строк
+	table = new PdfPTable(NameCellHat.length);
+	setHeader(table,NameCellHat); //задание заголовка (шапки таблицы)
 	 
-	 try {
-		document.add(table);
+}
+
+/**
+ * Метод получения ссылки на создаваемую таблицу, которую нужно наполнить (геттер).
+ * @return возвращает ссылку на созданную таблицу
+ */
+
+public PdfPTable getTable () {
+	return table;
+}
+
+/**
+ * Метод инициализации таблицы и добавления шапки в таблице.
+ * @param Table - ссылка на создаваемую таблицу, которая получена методом {@link InitTableAndAddHat}
+ * @param arrayCell - данные для строк в таблице (представляется в виде двумерного массива)
+ */
+
+public void addRowsInTable(PdfPTable Table, String[][] arrayCell) {
+	addRows(Table, arrayCell);
+	
+}
+
+/**
+ * Метод добавления таблицы в PDF документ.
+ * @param document - ссылка на создаваемый документ
+ * @param Table - ссылка на создаваемую таблицу, которая получена методом {@link InitTableAndAddHat}
+ */
+
+public void addTable(Document document, PdfPTable Table) {
+	try {
+		document.add(Table);
 	} catch (DocumentException e) {
 		e.printStackTrace();
 	}
 	
 }
+
+
 
 }
